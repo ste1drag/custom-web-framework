d12c37c24838bb8e2273ac0f8582db68ac5ec483
Stevan Dragović <stevandragovic.10@gmail.com>
2023-04-02 03:04:34 +0200

    Added hierarchy

--- src/models/Users.ts
+++ src/models/Users.ts
@@ -1,4 +1,7 @@
-import axios , { AxiosResponse } from "axios";
+import { Model } from './Model';
+import { Attributes } from './Attributes';
+import { ApiSync } from './ApiSync';
+import { Eventing } from './Eventing';
 
 interface UserProps {
     id?: number;
@@ -6,55 +9,14 @@ interface UserProps {
     age?: number;
 }
 
-type Callback = () => void;
+const rootUrl = 'http://localhost:3000/users';
 
-export class User {
-    events:{ [key: string]: Callback[] } = {};
-
-    constructor(private data: UserProps) {}
-
-    get(propName: string): number | string {
-        return this.data[propName];
-    }
-
-    set(update: UserProps): void {
-        Object.assign(this.data, update);
-    }
-
-    on(eventName: string, callback: Callback): void {
-        const handlers = this.events[eventName] || [];
-        handlers.push(callback);
-        this.events[eventName] = handlers;
-    }
-
-    trigger(eventName: string): void {
-        const handlers = this.events[eventName];
-
-        if(!handlers || handlers.length === 0) {
-            return;
-        }
-
-        handlers.forEach((callback) => {
-            callback();
-        })
-    }
-
-    fetch(): void {
-        axios
-            .get(`http://localhost:3000/users/${this.get('id')}`)
-            .then((response:AxiosResponse): void => {
-                this.set(response.data);
-            }) 
-    }
-
-    save(): void {
-        const id = this.get('id');
-
-        if (id) {
-            axios.put(`http://localhost:3000/users/${id}`,this.data);
-        } else {
-            axios.post('http://localhost:3000/users',this.data);
-        }
-
-    }
+export class User extends Model<UserProps> {
+   static buildUser(attrs: UserProps): User {
+    return new User(
+        new Attributes<UserProps>(attrs),
+        new Eventing(),
+        new ApiSync<UserProps>(rootUrl)
+    );
+   }
 }
